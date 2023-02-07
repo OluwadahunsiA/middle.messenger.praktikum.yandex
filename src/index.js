e54f74134b52366de5244c9499018b40da664139
OluwadahunsiA <tommydate0@gmail.com>
2023-02-07 13:11:40 +0300

    add block partial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Handlebars from 'handlebars';
 
+Handlebars.registerPartial('profile', 'user-information/userinformation');
+
 Handlebars.registerHelper('if', function (value) {
   if (value) {
     return value;
