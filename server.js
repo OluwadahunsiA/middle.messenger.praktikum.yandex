2c407b44d4de091d34dd21fad3216164aaf3a5fd
OluwadahunsiA <tommydate0@gmail.com>
2023-02-25 12:00:33 +0300

    edit confi

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require("express");
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 const port = 3000;
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Port ${port} is listening`);
